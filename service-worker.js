cdfd3ee1688d2c975db2dfb6cc6967fe26c6f5cb
Paps-maker <Livingstoneoduor21@gmail.com>
2025-06-20 20:53:24 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+    console.log('[PWA] Installed');
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
